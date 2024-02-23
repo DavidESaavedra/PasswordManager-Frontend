51c5a7b764bbe3875bda14466524645f1edbfc80
David Eloy Saavedra <davideloymartinez@gmail.com>
2024-02-22 20:47:36 -0800

    possible fix to vite config

--- vite.config.js
+++ vite.config.js
@@ -10,7 +10,7 @@ export default defineConfig({
       "/api": {
         // target: "http://localhost:5000/",
         target: "https://passwordmanager-backend.onrender.com/",
-        changeOrigin: true,
+        // changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
 
